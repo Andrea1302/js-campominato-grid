c95a4da3688eeb3d9a107b104c7d83b9b1afb02e
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-11-02 15:18:58 +0100

    Creazione griglie in base alla difficoltà

--- js/script.js
+++ js/script.js
@@ -15,3 +15,43 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro (
     // 2.1 in base al livello di difficoltà scelto, la griglia sarà diversa e quindi livello 1(facile) con 100 celle , livello 2 (medio) con 81 celle e livello 3(difficile) con 49 celle 
 
 // 3.quando l utente clicca su ogni cella , questa si colora di azzurro e avrà il numero relativo al suo interno 
+
+
+
+// Variabili globali 
+const containerSquare = document.getElementById("container_square");
+
+
+
+// 1.
+const sceltaDifficolta = parseInt(prompt("inserisci il livello di difficoltà ( '1' = Facile), ( '2' = Medio), ( '3' = Difficile)")) ;
+
+// 2.
+// 2.1
+
+// difficoltà 1 
+if ( sceltaDifficolta === 1 ) {
+    for ( let i = 0; i < 100; i++) {
+        var squareElement = document.createElement("div");
+        squareElement.classList.add("square_easy")
+        containerSquare.append(squareElement);
+    }
+}
+
+// difficoltà 2
+if ( sceltaDifficolta === 2 ) {
+    for ( let i = 0; i < 81; i++) {
+        var squareElement = document.createElement("div");
+        squareElement.classList.add("square_medium")
+        containerSquare.append(squareElement);
+    }
+}
+
+// difficoltà 3 
+if ( sceltaDifficolta === 3 ) {
+    for ( let i = 0; i < 49; i++) {
+        var squareElement = document.createElement("div");
+        squareElement.classList.add("square_extreme")
+        containerSquare.append(squareElement);
+    }
+}
