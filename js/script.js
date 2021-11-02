5420f48eb16086258c82b877d104f91747ad471a
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-11-02 14:42:50 +0100

    mia Logica esercizio commentata JS

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata,
+in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro (o simili, l’importante è dare all’utente il feedback che ha scoperto una casella che rimarrà scoperta, con il numero relativo).*/
+
+
+
+
+// 1.Chiedo all utente il livello di difficoltà per il gioco 
+    
+
+// 2. creazione griglia 
+    // 2.1 in base al livello di difficoltà scelto, la griglia sarà diversa e quindi livello 1(facile) con 100 celle , livello 2 (medio) con 81 celle e livello 3(difficile) con 49 celle 
+
+// 3.quando l utente clicca su ogni cella , questa si colora di azzurro e avrà il numero relativo al suo interno 
